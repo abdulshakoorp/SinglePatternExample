716dfbb67f26942c66048a5e285498d8c2a49079
Abdul Shakoor <abdulshakoorp@hotmail.com>
2021-06-07 10:59:53 +0400

    new project

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SingletonPatternExample1
+{
+    public class Calculator
+    {
+        private readonly Logger _logger;
+        public Calculator()
+        {
+            _logger = Logger.GetLogger();
+        }
+
+        public void Divide(int a, int b)
+        {
+            try
+            {
+                var result = a / b;
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SingletonPatternExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            calc.Divide(5, 0);
+            Console.ReadKey();
+        }
+    }
+}
